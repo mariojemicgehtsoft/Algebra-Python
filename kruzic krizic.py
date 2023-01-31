48757f2d0b966519e899062de0d7a5e628b0e3fd
MarioJemic <mario.jemic@gmail.com>
2023-01-31 20:15:06 +0100

    Update kruzic krizic.py

--- kruzic krizic.py
+++ kruzic krizic.py
@@ -1,6 +1,4 @@
 import os
-
-
 stanje=[[1,2,3],[4,5,6],[7,8,9]]
 adresa={1:[0,0], 2:[0,1], 3:[0,2], 4:[1,0], 5:[1,1], 6:[1,2], 7:[2,0], 8:[2,1], 9:[2,2]}
 def provjera():
@@ -25,26 +23,28 @@ def provjera():
        return stanje[0][2]                  
     else:
        return 0  
-       
 
 def ispis():
+
+    print ("-------")
     for red in stanje:
         n=0        
         for stupac in red:
             n=n+1
-            if n==3: 
-                  print(stupac  ) 
+            if  n==1 or n== 4 or n== 7:
+                 print(  "|" +str(stupac) + "|", end="") 
+            elif n==3: 
+                  print(str(stupac) + "|" ) 
             else:  
-                   print(stupac , end="") 
-
-
+                   print(str(stupac) + "|" , end="") 
+        print ("-------")             
+               
 os.system('cls')
 
 tko="x"
 while (True): 
       os.system('cls')
-      ispis() 
-        
+      ispis()         
       izbor= int(input(tko +" Izaberi Index : ")) 
       y=adresa[izbor][0] 
       x=adresa[izbor][1] 
@@ -52,10 +52,8 @@ while (True):
       if tko=="x":
          tko="o"
       elif tko=="o":
-         tko="x"
-      
+         tko="x"      
       pobjednik=provjera()
       if pobjednik != 0:
          print(str(pobjednik) + " pobjeduje")
          break
-
