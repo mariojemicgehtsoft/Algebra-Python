70521e539816fd5951c4b693e9eacdf96cdd6d62
MarioJemic <mario.jemic@gmail.com>
2023-01-31 21:18:23 +0100

    Update kruzic krizic.py

--- kruzic krizic.py
+++ kruzic krizic.py
@@ -2,6 +2,20 @@ import os
 vracanje_stanja=[[1,2,3],[4,5,6],[7,8,9]]
 stanje=[[1,2,3],[4,5,6],[7,8,9]]
 adresa={1:[0,0], 2:[0,1], 3:[0,2], 4:[1,0], 5:[1,1], 6:[1,2], 7:[2,0], 8:[2,1], 9:[2,2]}
+
+tko={1:"x", 2:"o"}
+def izbor_polja(who):
+    while True:
+          izbor= int(input(tko[who] +" Izaberi Index : ")) 
+          if who==1:
+            if izbor >0 and izbor < 10 and stanje[adresa[izbor][0]][adresa[izbor][1]]!=tko[2]:
+               break
+          else:
+            if izbor >0 and izbor < 10 and stanje[adresa[izbor][0]][adresa[izbor][1]]!=tko[1]:
+               break            
+
+
+    return izbor  
 def provjera():
     
     if stanje[0][0]== stanje[0][1] and stanje[0][1]== stanje[0][2]:
@@ -42,18 +56,22 @@ def ispis():
                
 os.system('cls')
 
-tko="x"
+who=tko[1]
 while (True): 
       os.system('cls')
-      ispis()         
-      izbor= int(input(tko +" Izaberi Index : ")) 
+      ispis()    
+
+      if who=="x": 
+         izbor= izbor_polja(1)
+      else: 
+         izbor= izbor_polja(2) 
       #y=adresa[izbor][0] x=adresa[izbor][1] 
       #stanje[       y       ][        x       ]=tko 
-      stanje[adresa[izbor][0]][adresa[izbor][1]]=tko
-      if tko=="x":
-         tko="o"
-      elif tko=="o":
-         tko="x"      
+      stanje[adresa[izbor][0]][adresa[izbor][1]]=who
+      if who=="x":
+         who="o"
+      elif who=="o":
+         who="x"      
       pobjednik=provjera()
       if pobjednik != 0:
          os.system('cls')
