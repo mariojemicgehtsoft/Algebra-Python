41a6e9cbb3bc8006754beeb49687c63edbd23732
MarioJemic <mario.jemic@gmail.com>
2023-01-31 21:15:40 +0100

    Update krizic kruzic pobjednicki uvjeti.py

--- krizic kruzic pobjednicki uvjeti.py
+++ krizic kruzic pobjednicki uvjeti.py
@@ -3,6 +3,19 @@ vracanje_stanja=[[1,2,3],[4,5,6],[7,8,9]]
 stanje=[[1,2,3],[4,5,6],[7,8,9]]
 adresa={1:[0,0], 2:[0,1], 3:[0,2], 4:[1,0], 5:[1,1], 6:[1,2], 7:[2,0], 8:[2,1], 9:[2,2]}
 pobjednicki_uvjet=[[1,2,3],[4,5,6],[7,8,9], [1,4,7], [2,5,8],   [3,6,7]    ,[3,5,7], [1,5,9]  ]
+tko={1:"x", 2:"o"}
+def izbor_polja(who):
+    while True:
+          izbor= int(input(tko[who] +" Izaberi Index : ")) 
+          if who==1:
+            if izbor >0 and izbor < 10 and stanje[adresa[izbor][0]][adresa[izbor][1]]!=tko[2]:
+               break
+          else:
+            if izbor >0 and izbor < 10 and stanje[adresa[izbor][0]][adresa[izbor][1]]!=tko[1]:
+               break            
+
+
+    return izbor       
 def provjera():
     winer=0
     for uvjet in pobjednicki_uvjet:
@@ -37,18 +50,22 @@ def ispis():
                
 os.system('cls')
 
-tko="x"
+who=tko[1]
 while (True): 
       os.system('cls')
-      ispis()         
-      izbor= int(input(tko +" Izaberi Index : ")) 
+      ispis()    
+
+      if who=="x": 
+         izbor= izbor_polja(1)
+      else: 
+         izbor= izbor_polja(2) 
       #y=adresa[izbor][0] x=adresa[izbor][1] 
       #stanje[       y       ][        x       ]=tko 
-      stanje[adresa[izbor][0]][adresa[izbor][1]]=tko
-      if tko=="x":
-         tko="o"
-      elif tko=="o":
-         tko="x"      
+      stanje[adresa[izbor][0]][adresa[izbor][1]]=who
+      if who=="x":
+         who="o"
+      elif who=="o":
+         who="x"      
       pobjednik=provjera()
       if pobjednik != 0:
          os.system('cls')
