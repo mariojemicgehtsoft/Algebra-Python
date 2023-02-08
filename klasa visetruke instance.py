c5521703e6ec5d4d58d9c52b699f91b1c74e2fba
MarioJemic <mario.jemic@gmail.com>
2023-02-09 00:13:23 +0100

    Update klasa visetruke instance.py

--- klasa visetruke instance.py
+++ klasa visetruke instance.py
@@ -17,4 +17,10 @@ for i in range(1,11):
     name = 'obj_{}'.format(i)
     my_objects[i]=MyClass(name,i)
 
+print(my_objects[4]) 
+print(my_objects[4].name) 
 print(my_objects[4].count) 
+
+print(my_objects[10]) 
+print(my_objects[10].name)  
+print(my_objects[10].count)  
