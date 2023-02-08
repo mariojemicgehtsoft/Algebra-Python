8837d0d09f7cd36f9b4be2a89ce7e0f7a02ad307
MarioJemic <mario.jemic@gmail.com>
2023-02-08 20:38:39 +0100

    Update klasa visetruke instance.py

--- klasa visetruke instance.py
+++ klasa visetruke instance.py
@@ -1,8 +1,12 @@
 class MyClass:
-    pass
+    def __init__(self, name):
+        self.name = name
+        self.pretty_print_name()
 
-objs = [MyClass() for i in range(10)]
-for obj in objs:
-    other_object.add(obj)
+    def pretty_print_name(self):
+        print("This object's name is {}.".format(self.name))
 
-objs[0].do_sth()
+my_objects = {}
+for i in range(1,11):
+    name = 'obj_{}'.format(i)
+    my_objects[name] = my_objects.get(name, MyClass(name = name))
