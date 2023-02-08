acb14cf76ea78ce785880c6627c7b1d95f9db8cb
MarioJemic <mario.jemic@gmail.com>
2023-02-09 00:11:00 +0100

    Update klasa visetruke instance.py

--- klasa visetruke instance.py
+++ klasa visetruke instance.py
@@ -1,12 +1,20 @@
 class MyClass:
-    def __init__(self, name):
+    def __init__(self, name, count=0):
         self.name = name
+        self.count=count
         self.pretty_print_name()
+
+    def __str__(self):
+        return f"{self.name}({self.count})"    
 
     def pretty_print_name(self):
         print("This object's name is {}.".format(self.name))
+  
 
 my_objects = {}
+count=0
 for i in range(1,11):
     name = 'obj_{}'.format(i)
-    my_objects[name] = my_objects.get(name, MyClass(name = name))
+    my_objects[i]=MyClass(name,i)
+
+print(my_objects[4].count) 
